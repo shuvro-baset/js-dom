5dd2150e225dd34491f73f9781a93acabcccc902
shuvro baset <md.abdul.baset75@gmail.com>
2021-08-18 23:56:27 +0600

    error handling

--- baap-er-bank/js/short.js
+++ baap-er-bank/js/short.js
@@ -1,15 +1,17 @@
 function myBank(inputId, updateAmount){
     let input = parseFloat(document.getElementById(inputId).value);
-    document.getElementById(inputId).value = "";
-    let currentAmount = parseFloat(document.getElementById(updateAmount).innerText) + input ;
-    document.getElementById(updateAmount).innerText = currentAmount;
-    let mainBalance = parseFloat(document.getElementById("balance-total").innerText);
-    if(inputId=='deposit-input'){
-        document.getElementById("balance-total").innerText = mainBalance  + input;
-    }
-    else{
-        document.getElementById("balance-total").innerText = mainBalance  - input;
-    }
+    if(input>0){
+        document.getElementById(inputId).value = "";
+        let currentAmount = parseFloat(document.getElementById(updateAmount).innerText) + input ;
+        document.getElementById(updateAmount).innerText = currentAmount;
+        let mainBalance = parseFloat(document.getElementById("balance-total").innerText);
+        if(inputId=='deposit-input'){
+            document.getElementById("balance-total").innerText = mainBalance  + input;
+        }
+        else{
+            document.getElementById("balance-total").innerText = mainBalance  - input;
+        }
+    } 
 }
 document.getElementById('deposit-button').addEventListener('click', function(){
     const deposit = myBank('deposit-input', 'deposit-total');
