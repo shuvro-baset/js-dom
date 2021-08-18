af03fe7558422b029bfe5fa3eda7ab4c5a51b3f4
shuvro baset <md.abdul.baset75@gmail.com>
2021-08-18 23:49:19 +0600

    short functionality done without error handling

--- baap-er-bank/js/short.js
+++ baap-er-bank/js/short.js
@@ -1,11 +1,19 @@
 function myBank(inputId, updateAmount){
     let input = parseFloat(document.getElementById(inputId).value);
     document.getElementById(inputId).value = "";
-    // let currentAmount = getElementById(updateAmount).innerText;
-    console.log(getElementById(updateAmount).innerText);
+    let currentAmount = parseFloat(document.getElementById(updateAmount).innerText) + input ;
+    document.getElementById(updateAmount).innerText = currentAmount;
+    let mainBalance = parseFloat(document.getElementById("balance-total").innerText);
+    if(inputId=='deposit-input'){
+        document.getElementById("balance-total").innerText = mainBalance  + input;
+    }
+    else{
+        document.getElementById("balance-total").innerText = mainBalance  - input;
+    }
 }
-
-
 document.getElementById('deposit-button').addEventListener('click', function(){
     const deposit = myBank('deposit-input', 'deposit-total');
+})
+document.getElementById('withdraw-button').addEventListener('click', function(){
+    const withdraw = myBank('withdraw-input', 'withdraw-total');
 })
